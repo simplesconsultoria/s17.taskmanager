33b31eeed61d2e811b3b7ff3125afb4726cf7bf0
Gustavo Lepri <lepri@simplesconsultoria.com.br>
2012-02-28 17:57:49 -0300

    locales improved

---
+++ src/s17/app/taskmanager/i18n/rebuildAll.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# ensure that when something is wrong, nothing is broken more than it should...
+set -e
+
+# first, create some pot containing anything
+i18ndude rebuild-pot --pot s17.app.taskmanager-plone.pot --create plone --merge plone-manual.pot ../*
+
+# finally, update the po files
+i18ndude sync --pot s17.app.taskmanager-plone.pot `find . -iregex '.*plone-.*\.po$'`
