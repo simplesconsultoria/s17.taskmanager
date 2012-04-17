0e2f327359ff808ea86cf4344dda8c3dc5f7b026
Gustavo Lepri <lepri@simplesconsultoria.com.br>
2012-04-17 12:04:02 -0300

    fixed erros in install dependencies

--- src/s17/app/taskmanager/upgrades/to1000.py
+++ src/s17/app/taskmanager/upgrades/to1000.py
@@ -11,6 +11,8 @@ from s17.app.taskmanager.config import PRODUCTS
 
 PROJECT = 's17.app.taskmanager'
 
+def get_package_name(name):
+    return name[9:] if name.startswith('Products') else name
 
 def fromZero(context):
     ''' Upgrade from Zero to version 1000
@@ -28,7 +30,9 @@ def fromZero(context):
         'collective.upload',
         ]
     # (name,locked,hidden,install,profile,runProfile)
-    dependencies = [(name,locked,hidden,profile) for name,locked,hidden,install,profile,runProfile in PRODUCTS if ((name in packages) and install)]
+    dependencies = [p for p in PRODUCTS
+                    if ((p['package'] in packages) and p['install'])]
 
-    for name,locked,hidden,profile in dependencies:
-        qi.installProduct(name, locked=locked, hidden=hidden, profile=profile)
+    for p in dependencies:
+        qi.installProduct(get_package_name(p['package']), locked=p['locked'],
+            hidden=p['hidden'], profile=p['profile'])
