5325382463f4dd484440196c1609e77d166aa73b
hvelarde <hector.velarde@gmail.com>
2012-05-11 15:26:22 -0300

    fixed package distribution

--- setup.py
+++ setup.py
@@ -1,63 +1,56 @@
 # -*- coding:utf-8 -*-
+
 import os
 from setuptools import setup, find_packages
 
-version = '0.1'
-
-long_description = open("README.rst").read() + "\n" +\
-                   open(os.path.join("docs", "INSTALL.txt")).read() + "\n" +\
-                   open(os.path.join("docs", "CREDITS.txt")).read() + "\n" +\
+version = '1.0'
+long_description = open("README.txt").read() + "\n" + \
+                   open(os.path.join("docs", "INSTALL.txt")).read() + "\n" + \
+                   open(os.path.join("docs", "CREDITS.txt")).read() + "\n" + \
                    open(os.path.join("docs", "HISTORY.txt")).read()
 
-
-# TODO: move this to another place
-def get_install_requirements():
-    """ XXX: document me!
-    """
-    import ConfigParser
-    path = os.path.join('src', 's17', 'app', 'taskmanager', 'dependencies.txt')
-    requirements = []
-    defaults = dict(version='')
-    config = ConfigParser.SafeConfigParser(defaults)
-    config.read([path])
-    for section in config.sections():
-        version = config.get(section, 'version')
-        if version and version[0].isdigit():
-            version = '==' + version
-        requirements.append('%s%s' % (section, version))
-    return requirements
 
 setup(name='s17.app.taskmanager',
       version=version,
       description="",
       classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Web Environment",
         "Framework :: Plone",
         "Framework :: Plone :: 4.1",
-        "Framework :: Zope2",
-        "Framework :: Zope3",
+        "Framework :: Plone :: 4.2",
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
         "Programming Language :: Python",
-        "Topic :: Internet :: WWW/HTTP",
-        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Programming Language :: Python :: 2.6",
         "Topic :: Software Development :: Libraries :: Python Modules",
         ],
       keywords='python plone zope simples_consultoria',
       author='Simples Consultoria',
       author_email='products@simplesconsultoria.com.br',
       url='http://www.simplesconsultoria.com.br',
-      license='gpl',
+      license='GPL',
       packages=find_packages('src'),
-      package_dir = {'': 'src'},
-      namespace_packages=['s17', 
-                         's17.app'],
+      package_dir={'': 'src'},
+      namespace_packages=['s17', 's17.app'],
       include_package_data=True,
       zip_safe=False,
-      install_requires=['setuptools','plone.namedfile[blobs]'] + get_install_requirements(),
+      install_requires=[
+        'setuptools',
+        'plone.app.dexterity>=1.2.1',
+        'plone.app.referenceablebehavior',
+        'plone.app.relationfield',
+        'plone.namedfile[blobs]',
+        'plone.principalsource',
+        'collective.watcherlist>=0.3',
+        'collective.upload>=1.0b1',
+        ],
       extras_require={
         'test': ['plone.app.testing'],
         },
       entry_points="""
-      # -*- Entry points: -*-
-
       [z3c.autoinclude.plugin]
       target = plone
       """,
