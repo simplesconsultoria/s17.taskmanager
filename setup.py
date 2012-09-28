125ede00efbed5db1401931cde1706ad86623f96
hvelarde <hector.velarde@gmail.com>
2012-09-27 21:35:36 -0300

    promote package status to Beta

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(name='s17.taskmanager',
       description="",
       long_description=long_description,
       classifiers=[
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 4 - Beta",
         "Environment :: Web Environment",
         "Framework :: Plone",
         "Framework :: Plone :: 4.2",
