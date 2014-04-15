82bae63756cd65dd3fd75260e38d6ca31a6d00ac
hvelarde <hector.velarde@gmail.com>
2014-04-15 19:40:55 -0300

    Remove needless i18n folder

--- src/s17/taskmanager/i18n/rebuildAll.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# ensure that when something is wrong, nothing is broken more than it should...
-set -e
-
-# first, create some pot containing anything
-i18ndude rebuild-pot --pot s17.taskmanager-plone.pot --create plone --merge plone-manual.pot ../*
-
-# finally, update the po files
-i18ndude sync --pot s17.taskmanager-plone.pot `find . -iregex '.*plone-.*\.po$'`
